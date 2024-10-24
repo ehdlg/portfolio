9d69aed9592ddd631a447a2f4b0026509ccef541
Enrique Hidalgo <121901959+ehdlg@users.noreply.github.com>
2024-10-24 19:46:28 +0200

    feat: add vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,13 @@ import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
   site: "https://ehdlg.github.io",
   base: "portfolio",
   output: "server",
+  adapter: vercel(),
 });
