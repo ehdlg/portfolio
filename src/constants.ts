1a7a3299b467659390944e08d6e92c0a57069134
Enrique Hidalgo <121901959+ehdlg@users.noreply.github.com>
2024-10-25 08:10:55 +0200

    feat: add light/theme constant, elements id constant

--- src/constants.ts
+++ src/constants.ts
@@ -1,1 +1,11 @@
 export const BASE_URL = ".";
+
+export const ELEMENT_ID = {
+  KNOB: "toggle-knob",
+  THEME: "toggle-dark",
+} as const;
+
+export const THEMES = {
+  DARK: "dark",
+  LIGHT: "light",
+} as const;
