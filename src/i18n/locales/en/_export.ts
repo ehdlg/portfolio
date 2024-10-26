8754428d63e9f58bda555141e1cb6b1180c706b9
Enrique Hidalgo <121901959+ehdlg@users.noreply.github.com>
2024-10-26 12:01:46 +0200

    feat(i18n): add and implement hero translations

--- src/i18n/locales/en/_export.ts
+++ src/i18n/locales/en/_export.ts
@@ -1,4 +1,5 @@
 import nav from "@i18n/locales/en/nav.json";
 import layout from "@i18n/locales/en/layout.json";
+import hero from "@i18n/locales/en/hero.json";
 
-export default { ...layout, ...nav } as const;
+export default { ...layout, ...nav, ...hero } as const;
