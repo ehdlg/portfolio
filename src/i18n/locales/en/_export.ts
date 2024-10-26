e2c0897c4d5ab4dee6194db85ffce8578bc1489b
Enrique Hidalgo <121901959+ehdlg@users.noreply.github.com>
2024-10-26 12:15:25 +0200

    feat(i18n): add and implement projects translations

--- src/i18n/locales/en/_export.ts
+++ src/i18n/locales/en/_export.ts
@@ -1,5 +1,9 @@
+import { projects as cvProjects } from "@data/cv.json";
 import nav from "@i18n/locales/en/nav.json";
 import layout from "@i18n/locales/en/layout.json";
 import hero from "@i18n/locales/en/hero.json";
+import jsonProjects from "@i18n/locales/en/projects.json";
 
-export default { ...layout, ...nav, ...hero } as const;
+const projects = { ...jsonProjects, "projects.projects": cvProjects };
+
+export default { ...layout, ...nav, ...hero, ...projects } as const;
