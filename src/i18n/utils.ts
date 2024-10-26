2fa8b36a0b88b25eaf511ea92fd9ce9e6743c67c
Enrique Hidalgo <121901959+ehdlg@users.noreply.github.com>
2024-10-26 08:38:04 +0200

    feat(i18n): add getLangFromulr and useTranslations helper functions

--- src/i18n/utils.ts
+++ src/i18n/utils.ts
@@ -4,3 +4,22 @@ export const languages = {
 };
 
 export const defaultLang = "es";
+
+export const ui: Record<keyof typeof languages, Record<string, string>> = {
+  es: {},
+  en: {},
+};
+
+export function getLangFromUrl(url: URL) {
+  const [, lang] = url.pathname.split("/");
+
+  if (lang in ui) return lang as keyof typeof ui;
+
+  return defaultLang;
+}
+
+export function useTranslations(lang: keyof typeof ui) {
+  return function t(key: keyof (typeof ui)[typeof defaultLang]) {
+    return ui[lang][key] || ui[defaultLang][key];
+  };
+}
