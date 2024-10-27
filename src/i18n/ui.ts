8522e69336857141830cf8e348e5d4b3a4b9fb30
Enrique Hidalgo <121901959+ehdlg@users.noreply.github.com>
2024-10-27 18:10:15 +0100

    feat(i18n): add emoji flags

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -1,9 +1,9 @@
 import en from "@i18n/locales/en/_export";
 import es from "@i18n/locales/es/_export";
 
-export const languages = {
-  es: "Español",
-  en: "English",
+export const languages: Record<keyof typeof ui, string> = {
+  es: "🇪🇸",
+  en: "🇬🇧",
 };
 export const defaultLang = "es";
 export const ui = {
