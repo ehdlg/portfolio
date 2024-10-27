3c6d074f48e6abaab244f382bc0a9d82d73f7e6c
Enrique Hidalgo <121901959+ehdlg@users.noreply.github.com>
2024-10-27 08:08:38 +0100

    feat(i18n): add contact translations

--- src/i18n/locales/en/_export.ts
+++ src/i18n/locales/en/_export.ts
@@ -3,7 +3,8 @@ import nav from "@i18n/locales/en/nav.json";
 import layout from "@i18n/locales/en/layout.json";
 import hero from "@i18n/locales/en/hero.json";
 import jsonProjects from "@i18n/locales/en/projects.json";
+import contact from "@i18n/locales/en/contact.json";
 
 const projects = { ...jsonProjects, "projects.projects": cvProjects };
 
-export default { ...layout, ...nav, ...hero, ...projects } as const;
+export default { ...layout, ...nav, ...hero, ...projects, ...contact } as const;
--- src/i18n/locales/es/_export.ts
+++ src/i18n/locales/es/_export.ts
@@ -3,7 +3,8 @@ import nav from "@i18n/locales/es/nav.json";
 import layout from "@i18n/locales/es/layout.json";
 import hero from "@i18n/locales/es/hero.json";
 import jsonProjects from "@i18n/locales/es/projects.json";
+import contact from "@i18n/locales/es/contact.json";
 
 const projects = { ...jsonProjects, "projects.projects": cvProjects };
 
-export default { ...layout, ...nav, ...hero, ...projects } as const;
+export default { ...layout, ...nav, ...hero, ...projects, ...contact } as const;
