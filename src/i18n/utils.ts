2f3232294beabc6d118110e0a30161f149b0566b
Enrique Hidalgo <121901959+ehdlg@users.noreply.github.com>
2024-10-27 09:09:17 +0100

    feat(i18n): add check language support function

--- src/i18n/utils.ts
+++ src/i18n/utils.ts
@@ -1,4 +1,4 @@
-import { ui, defaultLang } from "./ui";
+import { ui, defaultLang, languages } from "./ui";
 
 export function getLangFromUrl(url: URL) {
   const [, lang] = url.pathname.split("/");
@@ -12,4 +12,8 @@ export function useTranslations(lang: keyof typeof ui) {
   return function t(key: keyof (typeof ui)[typeof defaultLang]) {
     return ui[lang][key] || ui[defaultLang][key];
   };
+}
+
+export function checkLangSupport(lang: string) {
+  return lang in ui;
 }
