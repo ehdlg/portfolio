294e8b8ee8bfc5b21704f8723cb3651414ff06ea
Enrique Hidalgo <121901959+ehdlg@users.noreply.github.com>
2025-01-19 11:22:49 +0100

    feat(fonts): add and implment Nunito font

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,9 +1,14 @@
+import defaultTheme from "tailwindcss/defaultTheme";
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	darkMode: 'selector',
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  darkMode: "selector",
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Nunito Variable", ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [],
+};
