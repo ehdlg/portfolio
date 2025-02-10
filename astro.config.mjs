72ee991cb265e79d16ee2f40f64dd61127ce4d23
Enrique Hidalgo <121901959+ehdlg@users.noreply.github.com>
2025-02-10 09:08:25 +0100

    build: updgrade astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,14 +3,14 @@ import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
 
-import vercel from "@astrojs/vercel/serverless";
+import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
   base: ".",
   output: "server",
-  adapter: vercel(),
+  adapter: vercel({}),
   i18n: {
     defaultLocale: "es",
     locales: ["es", "en"],
